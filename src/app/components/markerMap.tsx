95673bf145181856f8913e5727026bfe300d799b
Evelyn Choukhman <100062745+evelynchoukhie@users.noreply.github.com>
2025-03-16 02:06:50 +1100

    feat: differentiate midpoint marker from origin markers (#6)

--- src/app/components/markerMap.tsx
+++ src/app/components/markerMap.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
-import { Marker } from "@react-google-maps/api";
 import { Coordinates } from "@/server/handlers/maps/search";
+import { Map, useMap, AdvancedMarker, Pin } from "@vis.gl/react-google-maps";
 
 const MarkerMap = ({
   coordinates,
@@ -12,71 +11,72 @@ const MarkerMap = ({
   midpoint: Coordinates;
   iterations: Coordinates[];
 }) => {
-  const { isLoaded } = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!,
-  });
+  const map = useMap();
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [map, setMap] = React.useState<google.maps.Map>();
+  React.useEffect(() => {
+    if (!map) return;
 
-  const onLoad = React.useCallback(
-    function callback(map: google.maps.Map) {
-      // Fit the map to the coordinates
-      if (coordinates.length > 0) {
-        const bounds = new window.google.maps.LatLngBounds();
-        [...coordinates, midpoint].forEach((coord) => {
-          bounds.extend({ lat: coord.latitude, lng: coord.longitude });
-        });
-        map.fitBounds(bounds);
-      }
-      setMap(map);
-    },
-    [coordinates, midpoint]
-  );
+    // Fit the map to the coordinates
+    if (coordinates.length > 0) {
+      const bounds = new window.google.maps.LatLngBounds();
+      [...coordinates, midpoint].forEach((coord) => {
+        bounds.extend({ lat: coord.latitude, lng: coord.longitude });
+      });
+      map.fitBounds(bounds);
+    }
+  }, [map, coordinates, midpoint]);
 
-  const onUnmount = React.useCallback(function callback() {
-    setMap(undefined);
-  }, []);
-
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerClassName="w-full h-screen"
-      center={{
-        lat: midpoint.latitude,
-        lng: midpoint.longitude,
-      }}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
+  return (
+    <Map
+      mapId="marker-map" 
+      colorScheme="FOLLOW_SYSTEM"
+      defaultZoom={10}
+      defaultCenter={{ lat: midpoint.latitude, lng: midpoint.longitude }}
+      gestureHandling={"greedy"} 
+      disableDefaultUI={true} 
+      className="w-full h-screen"
     >
-      {[...coordinates, midpoint].map((coord, index) => (
-        <Marker
-          key={index}
+      {coordinates.map((coord, index) => (
+        <AdvancedMarker
+          key={`coord-${index}`}
           position={{ lat: coord.latitude, lng: coord.longitude }}
-        />
+        >
+          <Pin
+            scale={1.25}
+            // Can change background and borderColor also
+            glyphColor={"#A71010"}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="size-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Pin>
+        </AdvancedMarker>
       ))}
+
+      <AdvancedMarker
+        position={{ lat: midpoint.latitude, lng: midpoint.longitude }}
+      />
+
       {iterations.map((iter, index) => (
-        <Marker
-          key={index}
+        <AdvancedMarker
+          key={`iter-${index}`}
           position={{ lat: iter.latitude, lng: iter.longitude }}
-          label={{
-            text: (index + 1).toString(),
-            color: "#000000",
-            fontSize: "12px",
-          }}
-          icon={{
-            path: google.maps.SymbolPath.CIRCLE,
-            fillColor: "#00FF00",
-            fillOpacity: 1,
-            strokeWeight: 0,
-            scale: 8,
-          }}
-        />
+        >
+          <div className="flex items-center justify-center bg-green-500 text-black font-bold rounded-full w-6 h-6 border border-white text-xs">
+            {index + 1}
+          </div>
+        </AdvancedMarker>
       ))}
-    </GoogleMap>
-  ) : (
-    <div>Loading...</div>
+    </Map>
   );
 };
 
