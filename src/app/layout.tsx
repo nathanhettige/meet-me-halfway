ea0a9c0c3f0612b4a639f3fa3e9dad92f3656187
Nathan Hettige <nathan.hettige@gmail.com>
2026-03-20 15:54:33 +1000

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next"
 import { Providers } from "./components/providers"
 
 import "./globals.css"
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "JStack App",
@@ -15,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className="antialiased">
         <Providers>{children}</Providers>
       </body>
